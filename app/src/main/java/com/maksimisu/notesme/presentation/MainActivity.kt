7a102f95ffa58a669f12656c10a7e8c8083b5be0
maksimisu <maksimisu@gmail.com>
2023-06-22 14:32:37 +0300

    initial

---
+++ app/src/main/java/com/maksimisu/notesme/presentation/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.maksimisu.notesme.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.maksimisu.notesme.presentation.ui.theme.NotesMETheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesMETheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
